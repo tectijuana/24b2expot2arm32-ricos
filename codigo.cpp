f88e65b2c59238279caba4d4cf52e00071f44f71
Ivan Jair Vizcarra Gomez <158106662+IvanVG7@users.noreply.github.com>
2024-03-20 15:33:25 -0600

    Update codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -1,3 +1,51 @@
+@  
+@  Title: Array de números ordenados por el método de burbuja   
+@  Filename: arrayBurbuja.s
+@  Autor: América Elizabeth Zavaleta Ramirez
+@  Date: 19/03/2024
+@  Description : 
+@  Imput: -
+@  Ouput: 
+@  
+@------------------------------------
+@   HIGH LEVEL LANGUAGE
+@------------------------------------
+@ Esta sección es donde se declaran y se inicializan las variables y los datos que se usarán en el programa.
+@
+@  // Primer programa de ejemplo en C++ solución
+@  
+@ #include<stdio.h>
+@ #include<stdlib.h>
+@ #include<time.h>
+@ int main(){
+    int vector[10],i,j,aux;
+    srand(time(NULL));
+    printf("Lista de numeros aleatorios:\n");
+    for(i=0;i<10;i++){
+        vector[i] = 100 + rand()% (110+1-100);
+        printf("%d ",vector[i]);
+    }
+    
+    
+    for(i=0;i<10;i++){
+        for(j=0;j<10;j++){
+            if(vector[j] > vector[j+1]){
+                aux = vector[j];
+                vector[j] = vector[j+1];
+                vector[j+1] = aux;
+            }
+        }
+    }
+    printf("\nLa lista de numeros ordenados por Burbuja es: \n");
+    for(i=0;i<10;i++){
+        printf("%d\n",vector[i]);
+    }
+    return 0;
+}
+
+
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
