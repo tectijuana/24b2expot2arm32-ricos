efa81bfaac86d4c03a1979c2783a2b4ad4696870
Eli-kopter <158227726+Eli-kopter@users.noreply.github.com>
2024-03-21 15:32:02 -0600

    Update array.s

--- code/array.s
+++ code/array.s
@@ -1,43 +1,57 @@
-@  
-@  Title: Array de números ordenados por el método de burbuja   
-@  Filename: arrayBurbuja.s
-@  Autor: América Elizabeth Zavaleta Ramirez
-@  Date: 19/03/2024
-@  Description : 
-@  Imput: -
-@  Ouput: 
-@  
-@------------------------------------
-@   HIGH LEVEL LANGUAGE
-@------------------------------------
-@
-@  // Primer programa de ejemplo en C++ 
-@
-@ #include<stdio.h>
-@ #include<stdlib.h>
-@ #include<time.h>
-@    int main(){
-@    int vector[10],i,j,aux;
-@    srand(time(NULL));
-@    printf("Lista de numeros aleatorios:\n");
-@    for(i=0;i<10;i++){
-@        vector[i] = 100 + rand()% (110+1-100);
-@        printf("%d ",vector[i]);
-@    }
-@    
-@    
-@    for(i=0;i<10;i++){
-@        for(j=0;j<10;j++){
-@            if(vector[j] > vector[j+1]){
-@                aux = vector[j];
-@                vector[j] = vector[j+1];
-@                vector[j+1] = aux;
-@            }
-@        }
-@    }
-@    printf("\nLa lista de numeros ordenados por Burbuja es: \n");
-@    for(i=0;i<10;i++){
-@        printf("%d\n",vector[i]);
-@    }
-@    return 0;
-@ }
+ 
+Title: Array de números ordenados por el método de burbuja   
+Filename: array.s
+Autor: América Elizabeth Zavaleta Ramirez
+Date: 19/03/2024
+Description : 
+Imput: -
+Ouput: 
+ 
+------------------------------------
+   HIGH LEVEL LANGUAGE
+------------------------------------
+
+// Programa de ejemplo en C++ 
+
+// Librerias
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+    int main()
+    {
+    // Declaración del array y variables auxiliares
+    int vector[10],i,j,aux;
+    srand(time(NULL));
+    printf("Lista de números aleatorios:\n");
+
+    // Generación de números de manera aleatoria
+    for(i=0;i<10;i++)
+    {
+       vector[i] = 100 + rand()% (110+1-100);
+        printf("%d ",vector[i]);
+    }
+    
+   // Ciclos FOR y condición IF para el ordenamiento Bubble Sort (Método de burbuja)
+   for(i=0;i<10;i++)
+   {
+        for(j=0;j<10;j++)
+        {
+            if(vector[j] > vector[j+1])
+            {
+                aux = vector[j];
+                vector[j] = vector[j+1];
+                vector[j+1] = aux;
+            }
+        }
+    }
+
+    // Imprime los números ordenados
+    printf("\nLa lista de números ordenados por Burbuja es: \n");
+    for(i=0;i<10;i++)
+    {
+        printf("%d\n",vector[i]);
+    }
+    
+    return 0;
+ }
